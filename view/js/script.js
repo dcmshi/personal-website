f650ccc4a01bf92e88d3afd38f056d847d3977bd
Shibisoma <shibisoma@hotmail.com>
2016-12-03 21:30:31 -0500

    remove lazyLoad

--- view/js/script.js
+++ view/js/script.js
@@ -21,16 +21,16 @@ function applyHeader()
 
 function lazyLoad(poContainer)
 {
-	var lstrSource   = poContainer.attr('data-src');
-	var lstrPosition = poContainer.attr('data-position');
+	// var lstrSource   = poContainer.attr('data-src');
+	// var lstrPosition = poContainer.attr('data-position');
 
-	$('<img>').attr('src', lstrSource).load(function()
-	{
-		poContainer.css('background-image', 'url("'+ lstrSource +'")');
-		poContainer.css('background-position', lstrPosition);
-		poContainer.css('-ms-filter', '"progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\')"');
-		poContainer.css('filter', 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\'');
-	});
+	// $('<img>').attr('src', lstrSource).load(function()
+	// {
+	// 	poContainer.css('background-image', 'url("'+ lstrSource +'")');
+	// 	poContainer.css('background-position', lstrPosition);
+	// 	poContainer.css('-ms-filter', '"progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\')"');
+	// 	poContainer.css('filter', 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\'');
+	// });
 }
 
 /* NAVIGATION FUNCTIONS */
